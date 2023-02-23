04b84c683822456f877d7655e4045ed615094517
Michael S <michaelsoares365@gmail.com>
2023-02-23 08:54:26 -0300

    CÓDIGO DO DESAFIO FÁBRICA DE CARROS #01

---
+++ fabrica_de_carros/src/desafio_fabrica_de_carros/model/CalculoDePreco.java
@@ -0,0 +1,48 @@
+package desafio_fabrica_de_carros.model;
+
+import java.text.DecimalFormat;
+
+public class CalculoDePreco {
+     private double valorCustoProducao;
+     private double porcetagemDistribuidor;
+     private double porcetagemImposto;
+     
+     public CalculoDePreco(double valorCustoProducao,double porcetagemDistribuidor,double porcetagemImposto) {
+    	 this.valorCustoProducao=valorCustoProducao;
+    	 this.porcetagemDistribuidor=porcetagemDistribuidor;
+    	 this.porcetagemImposto=porcetagemImposto;
+      }
+
+	public double getValorCustoProducao() {
+		return valorCustoProducao;
+	}
+
+	public void setValorCustoProducao(double valorCustoProducao) {
+		this.valorCustoProducao = valorCustoProducao;
+	}
+
+	public double getPorcetagemDistribuidor() {
+		return porcetagemDistribuidor;
+	}
+
+	public void setPorcetagemDistribuidor(double porcetagemDistribuidor) {
+		this.porcetagemDistribuidor = porcetagemDistribuidor;
+	}
+
+	public double getPorcetagemImposto() {
+		return porcetagemImposto;
+	}
+
+	public void setPorcetagemImposto(double porcetagemImposto) {
+		this.porcetagemImposto = porcetagemImposto;
+	}
+     
+    public String calculaPrecoFinal() {
+    	double precoFinal=((this.valorCustoProducao*this.porcetagemDistribuidor)/100)+((this.valorCustoProducao*this.porcetagemImposto)/100);
+    	precoFinal+=this.valorCustoProducao;
+    	DecimalFormat fmt=new DecimalFormat("##,###.000");
+    	String preco=fmt.format(precoFinal);
+    	return "Preço final: R$ "+precoFinal;
+    } 
+     
+}
